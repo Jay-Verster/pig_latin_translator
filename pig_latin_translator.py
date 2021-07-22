4d6b1adfbb09d4385b546fb72e7bf8a4ba235a80
Jay Verster <jeandre.verster@gmail.com>
2021-07-22 20:41:44 +0200

    Update pig_latin_translator.py

--- pig_latin_translator.py
+++ pig_latin_translator.py
@@ -12,19 +12,31 @@ original = input("\nPlease enter the sentence you would like translated.\nSenten
 # Notes: Creating a list by using the split() function on the "original" string
 original_list = original.split()
 
+# Notes: Below I create an empty list which will store the translated words.
 new_list = []
 
+# Notes: Creating a for-loop that will run through each word in original_list and translate and append it accordingly.
 for word in original_list:
+    
+    # Notes: if-statement that will add "yay" to the end of words in original_sentence that start with a vowel, and append it to new_list
     if word[0] in "aeiou":
         new_word = word + "yay"
         new_list.append(new_word)
+        
+    # Notes: An else-statement that will run in the case that the word doesn't start with a vowel. Each time the loop runs, a temporary variable called vowel_position
+    # with a 0 value is created. This will later be used to determine the index location of the first vowel in the word.
     else:
         vowel_position = 0
+        
+        # Notes: A for-loop that will itirate through each letter in words not starting with a vowel. For each consonant counted, the value of vowel_position is increased by 1.
+        # This will run continuously until the loop encounters the first vowel in the word.
         for letter in word:
             if letter not in "aeiou":
                 vowel_position = vowel_position + 1
             else:
                 break
+        # Notes: Temporary variables created withing the for-loop in order to create the translated word. The "consonant" variable saves the part of the word after the index
+        # position of "vowel_position, and "the_rest saves the part of the word up until the index value of "vowel_position".
         consonant = word[:vowel_position]
         the_rest = word[vowel_position:]
         new_word = the_rest + consonant + "ay"
@@ -33,7 +45,3 @@ for word in original_list:
 translated = " ".join(new_list)
 
 print(translated)
-
-# Those starting with a consonant or consonant cluster get split, added to the end of the word and the suffix "ay" is added.
-
-# Append all new altered words to a list, and convert the list into a string which is displayed to the user
